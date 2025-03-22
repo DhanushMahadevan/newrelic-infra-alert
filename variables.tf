fef6f35df59d13b7dc8ef33463a5d2aec2bb4548
Dhanush Mahadevan <123886042+DhanushMahadevan@users.noreply.github.com>
2025-03-22 10:20:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+################### NOTE: [ DEFAULT VALUES ARE SURRENDED BY DOUBLE QUOTES] ############################
+
+variable "api_key" {
+  default     =  "NRAK-LJ7HNM7SEV1JROF9O3T299LSAW3"
+"
+}
+
+variable "account_id" {
+  default     = "4197589
+"
+}
+
+variable "region" {
+  default     = "US" # Your region either US or EU
+}
+variable "entity_name" {
+  default = "az-pipleine-test"  # Must be an exact match to your application name in New Relic
+}
+
+variable "entity_domain" {
+  default = "INFRA" # BROWSER, INFRA, MOBILE, SYNTH, depending on your entity's domain
+}
+
+variable "entity_type" {
+  default = "HOST" # HOST, APPLICATION, etc.,.
+}
+
+variable "nrql_query" {
+default = "SELECT max(host.cpuPercent) AS 'CPU used %' FROM Metric"
+
+}
+
+variable "emails" {
+
+default = "dhanushmahadevanbalaji@gmail.com" # Place your emails separated by commas.
+  
+}
+
+variable "notify_triggers" {
+default =  ["ACTIVATED","PRIORITY_CHANGED","CLOSED"]
+
+}
