c3f87a6e2015e5b3097004270e8a2d62c995427d
Dhanush Mahadevan <123886042+DhanushMahadevan@users.noreply.github.com>
2025-03-25 16:00:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,11 +2,10 @@
 
 variable "api_key" {
 type = string
-default= "NRAK-LJ7HNM7SEV1JROF9O3T299LSAW3"
 }
 
 variable "account_id" {
-  default     = "4197589"
+  type =string
 }
 
 variable "region" {
